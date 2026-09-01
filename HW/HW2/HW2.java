19bd5b01c4772a8e68a48b1975a3bace4bc22610
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2026-09-01 12:20:26 -0400

    CPT session

---
+++ HW/HW2/HW2.java
@@ -0,0 +1,22 @@
+public class HW2 {
+    public static void main(String[] args) {
+
+    }
+}
+
+public class student {
+    String name;
+    int id;
+    double gpa;
+
+    public student(string name, int id, double gpa) {
+        this.gpa = gpa;
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printStudent() {
+
+    }
+
+}
